d428ea3861559cb9c006f8150b20dfd69fc4f9aa
djv0049 <44690969+djv0049@users.noreply.github.com>
2019-10-04 18:37:56 +1300

    finished classing

--- calcCorrelation.js
+++ calcCorrelation.js
@@ -1,112 +1,13 @@
-/*
-
-create x array[ 83, 116, 186, 81, 114]
-create y array[ 11.2, 9.3, 21.6, 6.9, 10.2]
-initialize x2 array[]
-initialize y2 array[]
-initialize xy array[]
-*/
-
-const xArr = [ 83, 116, 186, 81, 114]
-const yArr = [ 11.2, 9.3, 21.6, 6.9, 10.2]
-const n = xArr.length 
-
-
-//// functions
-
-// squared array calculator 
- function sqrArray (array, array2 = array) {
-  let result = [] 
-  for (let i in array) {
-    result.push(array[i]*array2[i])
-  }
-  return result
-}
-// array sum calculator
-function arraySum (array){
-  let result = 0
-  for (let num of array) {
-    result += num
-  }
-  return result
-} 
-// initialize calculated arrays 
-const x2Arr = sqrArray(xArr)
-const y2Arr = sqrArray(yArr)
-const xyArr = sqrArray(xArr, yArr)
-const xySum = arraySum(xyArr)
-const xSum = arraySum(xArr)
-const ySum = arraySum(yArr)
-
-
-
-//try to fit it all in one line 
-
-
-// get rid of duplication 
-// method to find the two numbers on the bottom of the array
-
-function bottomNums(sqrdArr, arrSum){ // bottomCorrelation 
-  return n * arraySum(sqrdArr) - arrSum ** 2
-}
-
-let topnum = n * xySum - xSum * ySum
-let bottom = (Math.sqrt(bottomNums(x2Arr, xSum) * bottomNums(y2Arr, ySum)))
-let rxy = topnum/bottom
-console.log("rxy = " + rxy) // showing r to the xy 
-//let answer = sqr(top/bottom)
-let answer = rxy ** 2
-
-console.log("the coeficient of the correlation is: " + answer)
-
-// regression
-// initialize values needed
-xArr
-yArr
-x2Arr
-xyArr
-const x2Sum = arraySum(x2Arr)
-
-
-// get average of array
-function findAvg(arr){
-  let l = arr.length
-  let result = 0
-  for(let n of arr){
-    result += n 
-  }
-  result /= n
-  return result 
-  
-}
-
-let xavg = findAvg(xArr)
-let yavg = findAvg(yArr)
-
-function regCalc (avg1, avg2 = avg1) {
-  return n * avg1 * avg2
-}
-
-let topreg = xySum - regCalc(xavg,yavg)
-let bottomreg = x2Sum - regCalc(xavg)
-
-let beta1 = topreg/bottomreg
-let beta0 = yavg - (beta1 * xavg)
-
-console.log("beta 1: " + beta1)
-console.log("beta 0: " + beta0)
-
 class CommonCalculations  {
   // common constants
-  constructor ( /*xArray, yArray*/  ){
-  this.xArr = [ 83, 116, 186, 81, 114]
-  this.yArr = [ 11.2, 9.3, 21.6, 6.9, 10.2] //xArray //[ 83, 116, 186, 81, 114]
-   // [ 11.2, 9.3, 21.6, 6.9, 10.2]
-  this.n = (this.xArr.length == this.yArr.length) ? this.xArr.length : 0
-  this.x2Arr = this.sqrArray(this.xArr)
-  this.y2Arr = this.sqrArray(this.yArr)
-  this.xyArr = this.sqrArray(this.xArr, this.yArr)
-  this.xySum = this.arraySum(this.xyArr)
+  constructor ( xArray, yArray  ){
+    this.xArr = xArray
+    this.yArr = yArray
+    this.x2Arr = this.sqrArray(this.xArr)
+    this.y2Arr = this.sqrArray(this.yArr)
+    this.xyArr = this.sqrArray(this.xArr, this.yArr)
+    this.xySum = this.arraySum(this.xyArr)
+    this.n = (this.xArr.length == this.yArr.length) ? this.xArr.length : 0
   }
   
   // common functions from both equations in order 
@@ -119,9 +20,9 @@ class CommonCalculations  {
     }
     return result
   }
-  arraySum = function (array) {
+  arraySum = function (arr) {
     let result = 0
-    for ( let num of array ) {
+    for ( let num of arr ) {
       result += num
     }
     return result
@@ -129,37 +30,72 @@ class CommonCalculations  {
 }
 
 class Correlation extends CommonCalculations {
+  
   constructor(x, y){
     super(x, y) 
+    this.xSum = this.arraySum(this.xArr)
+    this.ySum = this.arraySum(this.yArr)
+    this.dividend = 0
+    this.divisor = 0
+    this.rxy = 0 
+    this.coeficient = 0
+    this.calculateCorrelation()
   }
+  
+  calcDividend(sqrdArr, arrSum){ // bottomCorrelation 
+    return this.n * this.arraySum(sqrdArr) - arrSum ** 2
+  }
+  //
+  calculateCorrelation(){
+    let xSum = this.arraySum(this.xArr)
+    let ySum = this.arraySum(this.yArr)
+    this.dividend = this.n * this.xySum - xSum * ySum
+    this.divisor = Math.sqrt(this.calcDividend(this.x2Arr, xSum) * this.calcDividend(this.y2Arr, this.ySum))
+    this.rxy = this.dividend / this.divisor
+    this.coefficient = this.rxy ** 2
+    console.log("classed rxy = " + this.rxy) // works
+    console.log("coefficient = " + this.coefficient)
+  } 
 }
 
 class Regression extends CommonCalculations {
+  
   constructor (x, y){
     super (x, y)
+    this.beta0 = 0
+    this.beta1 = 0
+    this.calculateRegression()
+  }
+  
+  findAvg (arr) {
+    let n = this.n
+    let l = arr.length
+    let result = 0
+    for(let n of arr){
+      result += n 
+    }
+    result /= n
+    return result  
+  }
+  
+  betaCalc (avg1, avg2 = avg1) {
+    return this.n * avg1 * avg2
+  }
+  
+  calculateRegression () {
+    let x2Sum = this.arraySum(this.x2Arr)
+    let xAvg = this.findAvg(this.xArr)
+    let yAvg = this.findAvg(this.yArr)
+    let dividend = this.xySum - this.betaCalc(xAvg, yAvg)
+    let divisor = x2Sum - this.betaCalc(xAvg)
+    this.beta1 = dividend / divisor
+    this.beta0 = yAvg - (this.beta1 * xAvg)
+    console.log("beta0 = " + this.beta0)
+    console.log("beta1 = " + this.beta1)
   }
 }
 
 
-  // these methods are initialized outside of the class, and therefore have the "prototype" bit.
-  // commented out just in case they're needed again for demonstration later 
-  /*
-  ComCalc.prototype.sqrArray = function (array, array2 = array) {
-    let result = [] 
-    for (let i in array) {
-      result.push( array[i] * array2[i] )
-    }
-    return result
-  }
-  ComCalc.prototype.arraySum = function (array) {
-    let result = 0
-    for ( let num of array ) {
-      result += num
-    }
-    return result
-  }
-  */
-var r = new Regression()
-var c = new Correlation()
+
 
 // c = new Correlation([ 83, 116, 186, 81, 114], [ 11.2, 9.3, 21.6, 6.9, 10.2])
